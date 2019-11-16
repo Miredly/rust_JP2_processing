02529ef9137fd9c2f12aef081210ae99463fd6ba
Miles <miles@miredmusic.com>
2019-11-15 14:15:32 -1000

    working fully with only one image writeout stage

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,29 @@ extern crate kiss3d;
 extern crate nalgebra as na;
 extern crate voca_rs;
 
+use std::process::Command;
+use std::sync::{Mutex};
+use std::env;
 use imageproc::drawing::draw_text_mut;
 use image::{GenericImage, GenericImageView, Rgba};
 use image::imageops::overlay;
-
 use rusttype::{FontCollection, Scale};
 use glob::glob;
 use rayon::prelude::*;
-use std::env;
 use voca_rs::*;
-use std::process::Command;
 
+#[derive(Clone)]
 struct MetaData{
 	date: String,
 	time: String,
 	wlen: String
+}
+
+#[derive(Clone)]
+struct Frame {
+	frm: image::DynamicImage,
+	idx: i16,
+	dat: MetaData
 }
 
 fn build_list(wlen: String) -> Vec<String>{
@@ -36,8 +44,13 @@ fn build_list(wlen: String) -> Vec<String>{
 			Err(e)   => println!("{:?}", e),
 		}
 	}
-
 	return return_vec;
+}
+
+fn sort(mut flist : Vec<Frame>) -> Vec<Frame>{
+	println!("SORTING...");
+	flist.sort_by(|a, b| b.idx.cmp(&a.idx));
+	return flist;
 }
 
 fn parse_meta(file: String) -> MetaData {
@@ -70,23 +83,33 @@ fn apply_clut(mut img: image::DynamicImage, clut: image::DynamicImage) -> image:
 	return img;
 }
 
-fn annotate(mut frame: image::DynamicImage, metadata: MetaData) -> image::DynamicImage{
+fn annotate(mut frame: image::DynamicImage, metadata: &MetaData) -> image::DynamicImage{
 	let font   = Vec::from(include_bytes!("BebasNeue-Regular.ttf") as &[u8]);
     let font   = FontCollection::from_bytes(font).unwrap().into_font().unwrap();
-    let height = 60.0;
+    let height = 35.0;
     let scale  = Scale { x: height * 2.0, y: height };
-
-    draw_text_mut(&mut frame, Rgba([255u8, 255u8, 255u8, 0u8]), 3062, 310, scale, &font, &metadata.date);
-    draw_text_mut(&mut frame, Rgba([255u8, 255u8, 255u8, 0u8]), 3062, 310 + height as u32, scale, &font, &metadata.time);
+    //timestamp
+    draw_text_mut(&mut frame, Rgba([185u8, 185u8, 185u8, 0u8]), 3062, 312, scale, &font, &metadata.date);
+    draw_text_mut(&mut frame, Rgba([185u8, 185u8, 185u8, 0u8]), 3062, 312 + height as u32, scale, &font, &metadata.time);
+    //"earth for scale"
+    draw_text_mut(&mut frame, Rgba([185u8, 185u8, 185u8, 0u8]),  500, 2800, scale, &font, "earth for size scale");
 
 	return frame;
 }
 
 fn main(){
+	//clear tmp
+	Command::new("sh")
+		.arg("-c")
+		.arg(format!("rm -r tmp/ff/*.png"))
+		.spawn()
+		.expect("failed to execute process");
+
 	let args: Vec<String> = env::args().collect();
-	let target_dir = &args[1];
-	let output_dir = &args[2];
-	let wavlist    = ["94", "335", "211", "193", "171", "304"];
+	let target_dir        = &args[1];
+	let output_dir        = &args[2];
+	let wavlist           = ["94", "335", "211", "193", "171", "304"];
+	let mut wlist         = Vec::new(); 
 
 	for wlen in wavlist.iter(){
 		let list = build_list(target_dir.to_string() + "/" + wlen);
@@ -97,110 +120,87 @@ fn main(){
 		}
 		println!("FRAMES: {:#?} TARGET: {:#?} OUTPUT: {:#?}", indices.len(), target_dir, output_dir);
 
-		let img_path_len = list[0].split("/").collect::<Vec<_>>().len(); 
+		let img_path_len        = list[0].split("/").collect::<Vec<_>>().len(); 
+		let frames : Vec<Frame> = Vec::new();
+		let mframes             = Mutex::new(frames);
 
 		list.par_iter().zip(indices).for_each(|(item, index)|{
 			println!("ITEM {} INDEX {}", item, index);
 			let in_path   = item.to_string(); 
 		    let meta_data = parse_meta(in_path.split("/").collect::<Vec<_>>()[img_path_len - 1].to_string());
-		    let out_path  = "tmp/".to_owned() + &meta_data.wlen.to_owned() + "/colored/" + &index.to_string() + ".png" ;
 
 		    //colorize and add metadata as text to the frame
-		    let img  = open_jp2(in_path.to_string());
-		    let clut = image::open("media/colortables/".to_owned() + &meta_data.wlen + "_color_table.png").unwrap();
+		    let img   = open_jp2(in_path.to_string());
+		    let clut  = image::open(format!("media/colortables/{}_color_table.png", meta_data.wlen)).unwrap();
+		    let img   = apply_clut(img, clut);
+		    let frame = Frame{
+		    	frm: img, 
+		    	idx: index, 
+		    	dat: meta_data
+		    };
 
-		    let img = apply_clut(img, clut);
-		    let img = annotate(img, meta_data);
-
-		    img.save(out_path).unwrap();
+		    mframes.lock().unwrap().push(frame);
 
 		});
+		wlist.push(mframes);
+
 	}
 
-	//Take a second pass to build out our finalized frames with colored images- this requires a second iteration through all the colored frames because each final frame needs a thumbnail from all the other spectra
+	//bring our colored frames out of the mutex
+	let mut cframes = Vec::new();
+	for wlen in wlist.iter().rev(){
+		let freed = sort(wlen.lock().unwrap().to_vec());
+		cframes.push(freed);
+	}
+
+	let frames : Vec<image::DynamicImage> = Vec::new();
+	let f_frames                          = Mutex::new(frames);
+	let mut l_idx                         = 0;
 	
-	for (idx, wlen) in wavlist.iter().enumerate(){
-
-		let mut list : Vec<String> = vec!("init".to_string());
-		let globtarget = &("tmp/".to_owned() + &(wlen.to_owned().to_owned() + "/colored/*.png"));
-		println!("GLOBTARGET: {}", globtarget);
-		list.pop();
-		for entry in glob(globtarget).expect("Failed to read glob pattern"){
-			match entry{
-				Ok(path) => list.push(path.display().to_string()),
-				Err(e)   => println!("{:?}", e),
-			}
-		}
-
-		let mut indices : Vec<i16> = Vec::new();
+	//build our final composite frames
+	for wlen in cframes.iter(){
 		
-		for i in 0..list.len(){
-			indices.push(i as i16);
-		}
-		println!("FINISHING: {:#?} TARGET: {:#?}", indices.len(), wlen);
-		list.par_iter().zip(indices).for_each(|(item, index)|{
-			
-			let out_path = "tmp/".to_owned() + &wlen.to_owned() + "/finished/" + &manipulate::zfill(&index.to_string(), 4) + ".png";
-			
-
+		wlen.par_iter().for_each(|item|{
+			println!("CHECK: : {} :: {}", manipulate::zfill(&item.idx.to_string(), 2), item.dat.wlen);
 		    //build all the additional images to add to the frame
-		    // let mut frame = image::open("media/misc/TEMPLATE_2x3.png").unwrap();
 		    let mut frame = image::DynamicImage::new_rgb8(3840, 3240);
-		    let sun       = image::open(item.to_string()).unwrap().resize(3240, 3240, image::FilterType::Nearest);
+		    let sun       = item.frm.resize(3240, 3240, image::FilterType::Nearest);
 		    let earth     = image::open("media/misc/earth.png").unwrap();
-		    let gfx       = image::open(format!("media/misc/OVERLAY_2x3_WHITE_{}.png", idx)).unwrap();
-		    let thumb94   = image::open("tmp/94/colored/".to_owned() + &index.to_string() + ".png").unwrap().resize(180, 180, image::FilterType::Nearest);
-		    let thumb304  = image::open("tmp/304/colored/".to_owned() + &index.to_string() + ".png").unwrap().resize(180, 180, image::FilterType::Nearest);
-		    let thumb171  = image::open("tmp/171/colored/".to_owned() + &index.to_string() + ".png").unwrap().resize(180, 180, image::FilterType::Nearest);
-		    let thumb193  = image::open("tmp/193/colored/".to_owned() + &index.to_string() + ".png").unwrap().resize(180, 180, image::FilterType::Nearest);
-		    let thumb211  = image::open("tmp/211/colored/".to_owned() + &index.to_string() + ".png").unwrap().resize(180, 180, image::FilterType::Nearest);
-		    let thumb335  = image::open("tmp/335/colored/".to_owned() + &index.to_string() + ".png").unwrap().resize(180, 180, image::FilterType::Nearest);
-
-		    println!("ADDING OVERLAY: {}", out_path);
+		    let gfx       = image::open(format!("media/misc/OVERLAY_2x3_WHITE_{}.png", l_idx)).unwrap();
+		    let thumb304  = cframes[0][item.idx as usize].frm.resize(180, 180, image::FilterType::Nearest);
+		    let thumb171  = cframes[1][item.idx as usize].frm.resize(180, 180, image::FilterType::Nearest);
+		    let thumb193  = cframes[2][item.idx as usize].frm.resize(180, 180, image::FilterType::Nearest);
+		    let thumb211  = cframes[3][item.idx as usize].frm.resize(180, 180, image::FilterType::Nearest);
+		    let thumb335  = cframes[4][item.idx as usize].frm.resize(180, 180, image::FilterType::Nearest);
+		    let thumb94   = cframes[5][item.idx as usize].frm.resize(180, 180, image::FilterType::Nearest);
 
 		    //add additional images to main frame
-		    overlay(&mut frame, &sun,      345, 0 );
-		    overlay(&mut frame, &earth,    250, 2750);
-		    overlay(&mut frame, &thumb94,   88, 650 );
-		    overlay(&mut frame, &thumb335,  88, 875 );
-		    overlay(&mut frame, &thumb211,  88, 1105);
-		    overlay(&mut frame, &thumb193,  88, 1335);
-		    overlay(&mut frame, &thumb171,  88, 1560);
-		    overlay(&mut frame, &thumb304,  88, 1790);
-		    overlay(&mut frame, &gfx,        0, 0   );
+		    overlay(&mut frame, &sun,      345, 0    );
+		    overlay(&mut frame, &earth,    500, 2750 );
+		    overlay(&mut frame, &thumb94,   88, 650  );
+		    overlay(&mut frame, &thumb335,  88, 875  );
+		    overlay(&mut frame, &thumb211,  88, 1105 );
+		    overlay(&mut frame, &thumb193,  88, 1335 );
+		    overlay(&mut frame, &thumb171,  88, 1560 );
+		    overlay(&mut frame, &thumb304,  88, 1790 );
+		    overlay(&mut frame, &gfx,        0, 0    );
+
+		    let frame = annotate(frame, &item.dat);
 
 		    //add our processed frame to the tmp directory
-		    frame.save(out_path).unwrap();
+		    f_frames.lock().unwrap().push(frame);
 		});
+		l_idx += 1;
 	}
 
-	// build the final video
-	let oname = "output_video.mp4";
-	let mut flist : Vec<String> = Vec::new();
-
-	for wlen in wavlist.iter().rev(){
-		for entry in glob(&format!("tmp/{}/finished/*.png", wlen).to_string()).expect("Failed to read glob pattern"){
-			match entry{
-				Ok(path) => flist.push(path.display().to_string()),
-				Err(e)   => println!("{:?}", e),
-			}
-		}
-	}
-	
-	for (i, f) in flist.iter().enumerate(){
-		let o = format!("tmp/ff/{}.png", manipulate::zfill(&i.to_string(), 4));
-		Command::new("sh")
-			.arg("-c")
-			.arg(format!("cp {} {}", f, o))
-			.output()
-			.expect("failed to execute process");
-
+	for (idx, frame) in f_frames.lock().unwrap().iter().enumerate(){
+		frame.save(format!("tmp/ff/{}.png", manipulate::zfill(&idx.to_string(), 4))).unwrap();
 	}
 
+	// build a video
 	Command::new("sh")
 		.arg("-c")
-		.arg(format!("ffmpeg -r 24 -i tmp/ff/%04d.png -vcodec libx264 -filter 'minterpolate=mi_mode=blend' -b:v 4M -pix_fmt yuv420p  -y {}/{}", output_dir, oname))
+		.arg(format!("ffmpeg -r 24 -i tmp/ff/%04d.png -vcodec libx264 -filter 'minterpolate=mi_mode=blend' -b:v 4M -pix_fmt yuv420p  -y {}/{}", output_dir, "testvideo.mp4"))
 		.spawn()
 		.expect("failed to execute process");
-
 }
